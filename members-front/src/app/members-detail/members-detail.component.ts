80b044e0cae054d42f921e85726522e81e19634f
unknown <murakami.bruno@gmail.com>
2021-01-31 00:06:50 -0300

    Implementação Delete

--- members-front/src/app/members-detail/members-detail.component.ts
+++ members-front/src/app/members-detail/members-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { AppComponent } from '../app.component';
 import { ApiService } from './api.service';
 
 
@@ -13,7 +14,9 @@ export class MembersDetailComponent implements OnInit {
   constructor(
     private route: ActivatedRoute, 
     private api: ApiService,
-    private router: Router) { }
+    private router: Router,
+    private appComponent: AppComponent
+    ) { }
     
   selectedMember = {id: 0, name: '', surname: '', phone: ''};
   selectedId;
@@ -50,5 +53,21 @@ export class MembersDetailComponent implements OnInit {
 
   newMember() {
     this.router.navigate(['new-member'])
+  }
+
+  delete() {
+    this.api.deleteMember(this.selectedId).subscribe(
+      data => {
+        this.appComponent.members.forEach((elem,index) => {
+          if (elem.id == this.selectedId) {
+            this.appComponent.members.splice(index,1);
+          }
+        });
+
+      },
+      error => {
+        console.log("Aconteceu um erro", error.message);
+      }
+    );
   }
 }
